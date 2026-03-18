4f29186598ba913ccd9a2961a4c47849f3ada0e0
unknown <seandanielrooney@gmail.com>
2026-03-18 14:41:45 -0700

    reverting to original to see if link build error persists

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,6 @@ export default function Header({ onSignIn }: { onSignIn: () => void }) {
     <header className="site-header">
       <div className="container header-inner">
         <div className="brand">
-          <a href="/" title="To Home Page" className="flex items-center">
           <div className="brand-mark" aria-hidden="true">
             <img
               src="/logo.jpeg"
@@ -31,7 +30,6 @@ export default function Header({ onSignIn }: { onSignIn: () => void }) {
             />
           </div>
           <span className="brand-name">Try Local</span>
-          </a>
           <span className="brand-subtle">Gresham, OR</span>
         </div>
 
