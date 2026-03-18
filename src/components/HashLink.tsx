8fce633d9e3bcaccbcc3c0e01c17952d42cc6faf
unknown <seandanielrooney@gmail.com>
2026-03-18 15:35:21 -0700

    lets try it again

--- src/components/HashLink.tsx
+++ src/components/HashLink.tsx
@@ -1,14 +1,18 @@
-'use client'
 // This is so you can have a link that can both change page and go to an ID - used in global elements like Navbar and Footer.
-
+'use client'
+import React from 'react'
 import { useRouter, usePathname } from 'next/navigation'
 
-export function HashLink({ href, ...props }) {
+interface HashLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  href: string
+}
+
+export function HashLink({ href, ...props }: HashLinkProps) {
   const router = useRouter()
   const pathname = usePathname()
   const [path, hash] = href.split('#')
 
-  const handleClick = (e) => {
+  const handleClick = (e: React.MouseEvent) => {
     e.preventDefault()
     if (pathname === (path || '/')) {
       document.getElementById(hash)?.scrollIntoView({ behavior: 'smooth' })
